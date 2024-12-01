5df7a8d5fe0e3ebab62f7993adf9d4083e47807d
Emma Christy <emma.t.christy@gmail.com>
2024-12-01 07:16:48 +0200

    added getting input file

---
+++ advent_of_code_2024/day_1_pt_1_loc_diff.c
@@ -0,0 +1,15 @@
+/*
+ - get input in 2 arrays
+ - sort the 2 arrays then get the difference between them
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+	}
+}
