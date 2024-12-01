9448b48cad6ffaf2a88fff40ffabf201902ca2c5
Emma Christy <emma.t.christy@gmail.com>
2024-12-01 07:31:18 +0200

    added parsing and storing input numbers

--- advent_of_code_2024/day_1_pt_1_loc_diff.c
+++ advent_of_code_2024/day_1_pt_1_loc_diff.c
@@ -4,12 +4,29 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
+	int left_nums[1000];
+	int right_nums[1000];
+	char *save_ptr = NULL;
+	char *val = NULL;
+	int line_count = 0;
+
 	char *input_line = NULL;
 	size_t lim = 0;
 
 	while(getline(&input_line, &lim, stdin) > 1){
+		val = strtok_r(input_line, " \n", &save_ptr);
+		left_nums[line_count] = atoi(val);
+
+		val = strtok_r(NULL, " \n", &save_ptr);
+		right_nums[line_count] = atoi(val);
+
+		++line_count;
 	}
+
+	free(input_line);
+	return 0;
 }
