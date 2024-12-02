c3b83acddae26d171c60f876f4bc0c69b53e4dde
Emma Christy <emma.t.christy@gmail.com>
2024-12-02 07:11:43 +0200

    added day 2 part 1 file

---
+++ advent_of_code_2024/day_2_pt_1_red_nosed_reactor.c
@@ -0,0 +1,5 @@
+/*
+ * check if the first 2 numbers of the report are increasing or decreasing
+ * then check if they are doing so at safe levels
+ * then check if the rest of the report is doing so according to the rules
+*/
