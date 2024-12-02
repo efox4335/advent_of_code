a8beaaae5e9f0bed01cb5ab3ac7d165f602217ff
Emma Christy <emma.t.christy@gmail.com>
2024-12-02 08:41:54 +0200

    added rechecking of report without bad level

--- advent_of_code_2024/day_2_pt_2_red_nosed_reactor.c
+++ advent_of_code_2024/day_2_pt_2_red_nosed_reactor.c
@@ -4,13 +4,15 @@
  * then check if they are doing so at safe levels
  * then check if the rest of the report is doing so according to the rules
  * if unsafe data is found check if the report would be safe if either of the unsafe data pair would be removed
+ * check also if removing levle at index 1 or 0 fixes it as they set weather the data increases or decreases
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 //returns index of unsafe level on fail report_len on success
-int report_safe(int *report, int report_len)
+//will skip skip_index in pocessing
+int report_safe(int *report, int report_len, int skip_index)
 {
 	enum{INC, DEC};
 
@@ -18,8 +20,16 @@ int report_safe(int *report, int report_len)
 	int level_2 = 0;
 	int report_index = 0;
 
+	if(report_index == skip_index){
+		++report_index;
+	}
+
 	level_1 = report[report_index];
 	++report_index;
+	if(report_index == skip_index){
+		++report_index;
+	}
+
 	level_2 = report[report_index];
 	++report_index;
 
@@ -41,15 +51,24 @@ int report_safe(int *report, int report_len)
 			return report_index - 1;
 	}
 
+	if(report_index == skip_index){
+			++report_index;
+	}
+
 	if(report_type == DEC){
 		while(report_index < report_len){
 			level_1 = level_2;
 			level_2 = report[report_index];
 			++report_index;
+
 			level_dif = level_1 - level_2;
 
 			if(level_dif <= 0 || level_dif >= 4){
 				return report_index - 1;
+			}
+
+			if(report_index == skip_index){
+				++report_index;
 			}
 		}
 	}else{
@@ -60,7 +79,11 @@ int report_safe(int *report, int report_len)
 			level_dif = level_1 - level_2;
 
 			if(level_dif >= 0 || level_dif <= -4){
-				return report_index - 1;;
+				return report_index - 1;
+			}
+
+			if(report_index == skip_index){
+				++report_index;
 			}
 		}
 	}
@@ -72,10 +95,12 @@ int main(void)
 {
 	char *str = NULL;
 	size_t lim = 0;
+	int safe_report_count = 0;
 
 	while(getline(&str, &lim, stdin) > 1){
 		int report_arr[100];
 		int level_count = 0;
+		int unsafe_level = 0;
 
 		char *level_ptr = NULL;
 		char *save_ptr = NULL;
@@ -88,7 +113,23 @@ int main(void)
 
 			level_ptr = strtok_r(NULL, delim, &save_ptr);
 		}while(level_ptr != NULL);
+
+		if((unsafe_level = report_safe(report_arr, level_count, level_count)) != level_count){//check if report is safe for both removed levels
+			if(report_safe(report_arr, level_count, unsafe_level) == level_count){
+				++safe_report_count;
+			}else if(report_safe(report_arr, level_count, unsafe_level - 1) == level_count){
+				++safe_report_count;
+			}else if(report_safe(report_arr, level_count, 0) == level_count){
+				++safe_report_count;
+			}else if(report_safe(report_arr, level_count, 1) == level_count){
+				++safe_report_count;
+			}
+		}else{
+			++safe_report_count;
+		}
 	}
+
+	printf("%d\n", safe_report_count);
 
 	free(str);
 	return 0;
