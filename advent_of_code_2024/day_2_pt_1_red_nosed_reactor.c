1ca9c748c24131f9556d9802ec18ea371b9121de
Emma Christy <emma.t.christy@gmail.com>
2024-12-02 07:14:15 +0200

    gets input

--- advent_of_code_2024/day_2_pt_1_red_nosed_reactor.c
+++ advent_of_code_2024/day_2_pt_1_red_nosed_reactor.c
@@ -3,3 +3,17 @@
  * then check if they are doing so at safe levels
  * then check if the rest of the report is doing so according to the rules
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *str = NULL;
+	size_t lim = 0;
+
+	while(getline(&str, &lim, stdin) > 1){
+	}
+
+	free(str);
+	return 0;
+}
