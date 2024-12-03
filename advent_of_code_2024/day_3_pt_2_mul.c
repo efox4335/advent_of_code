7e3c53a39beab646be369609d65246c1058d121d
Emma Christy <emma.t.christy@gmail.com>
2024-12-03 07:17:33 +0200

    added finding of all substrings matching mul(

--- advent_of_code_2024/day_3_pt_2_mul.c
+++ advent_of_code_2024/day_3_pt_2_mul.c
@@ -2,13 +2,22 @@
  * scan through looking for string "mul(" then check if both numers are good then multiply them
 */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
+	char find[] = "mul(";
 	char *input_line = NULL;
 	size_t lim = 0;
+	char *mul_ptr = NULL;
 
 	while(getline(&input_line, &lim, stdin) > 1){
+		mul_ptr = strstr(input_line, find);
+
+		while(mul_ptr != NULL){
+			mul_ptr = strstr(mul_ptr + 1, find);//skips past the last found "mul("
+		}
 	}
 
 	free(input_line);
