88a1e61aa98d673b00be903bafec15eea7e02784
Emma Christy <emma.t.christy@gmail.com>
2024-12-03 08:50:09 +0200

    added checking of do and don't instructions

--- advent_of_code_2024/day_3_pt_2_mul.c
+++ advent_of_code_2024/day_3_pt_2_mul.c
@@ -59,20 +59,45 @@ num_error:
 int main(void)
 {
 	char find[] = "mul(";
+	char do_find[] = "do()";
+	char dont_find[] = "don't()";
 	char *input_line = NULL;
 	size_t lim = 0;
 	char *mul_ptr = NULL;
+	char *dont_ptr = NULL;
 	int arg_1 = 0;
 	int arg_2 = 0;
 	int prod_sum = 0;
+	int in_dont = 0;//used to check if the last line ended in a don't block
 
 	while(getline(&input_line, &lim, stdin) > 1){
-		mul_ptr = strstr(input_line, find);
+		if(in_dont == 0){
+			mul_ptr = strstr(input_line, find);
+			dont_ptr = strstr(input_line, dont_find);
+		}else{
+			mul_ptr = strstr(input_line, do_find);
+			if(mul_ptr != NULL){
+				dont_ptr = strstr(mul_ptr, dont_find);//if there is a don't before the next do when the last line already ended in a don't
+				in_dont = 0;
+			}else{//if there is no do() instruction in the line
+				continue;
+			}
+		}
 
 		while(mul_ptr != NULL){
-			get_nums(mul_ptr, &arg_1, &arg_2);
-			prod_sum += arg_1 * arg_2;
-			mul_ptr = strstr(mul_ptr + 1, find);//skips past the last found "mul("
+			if(dont_ptr == NULL || mul_ptr < dont_ptr){
+				get_nums(mul_ptr, &arg_1, &arg_2);
+				prod_sum += arg_1 * arg_2;
+				mul_ptr = strstr(mul_ptr + 1, find);//skips past the last found "mul("
+			}else{//if mul_ptr is past the dont_ptr
+				mul_ptr = strstr(dont_ptr, do_find);//can't look from mul_ptr because there might be a do instruction before the next mul instruction after the don't instruction
+				if(mul_ptr == NULL){
+					in_dont = 1;
+				}else{
+					mul_ptr = strstr(mul_ptr, find);//next mul after do instruction
+					dont_ptr = strstr(mul_ptr, dont_find);
+				}
+			}
 		}
 	}
 
