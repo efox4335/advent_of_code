e6e3e14fea73dfbea6f95ac618c6fcb97dc5c1b6
Emma Christy <emma.t.christy@gmail.com>
2024-12-03 08:14:24 +0200

    added getting of all numbers that are part of and mul() function

--- advent_of_code_2024/day_3_pt_2_mul.c
+++ advent_of_code_2024/day_3_pt_2_mul.c
@@ -4,6 +4,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+//puts chars in buff as long as they are didigts and null terminates buff
+//returns the length of the int
+int num_to_str(char *input, char *buff)
+{
+	int index = 0;
+
+	for(index = 0; isdigit(input[index]); ++index){
+		buff[index] = input[index];
+	}
+
+	buff[index] = '\0';
+	return index;
+}
+
+//sets both arg_1 and arg_2 to 0 on fail
+void get_nums(char *input, int *arg_1, int *arg_2)
+{
+	char buff[4];
+	int num_len;
+
+	num_len = num_to_str(input + 4, buff);
+	if(num_len <= 0 || num_len >= 4){
+		goto num_error;
+	}
+	*arg_1 = atoi(buff);
+
+	num_len = num_to_str(input + 4 + num_len + 1, buff);
+	if(num_len <= 0 || num_len >= 4){
+		goto num_error;
+	}
+	*arg_2 = atoi(buff);
+
+	return;
+
+num_error:
+	*arg_1 = 0;
+	*arg_2 = 0;
+}
 
 int main(void)
 {
@@ -11,11 +51,14 @@ int main(void)
 	char *input_line = NULL;
 	size_t lim = 0;
 	char *mul_ptr = NULL;
+	int arg_1 = 0;
+	int arg_2 = 0;
 
 	while(getline(&input_line, &lim, stdin) > 1){
 		mul_ptr = strstr(input_line, find);
 
 		while(mul_ptr != NULL){
+			get_nums(mul_ptr, &arg_1, &arg_2);
 			mul_ptr = strstr(mul_ptr + 1, find);//skips past the last found "mul("
 		}
 	}
