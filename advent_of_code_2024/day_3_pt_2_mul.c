c484174ffa1c152239213ebdc665f9272817ca8b
Emma Christy <emma.t.christy@gmail.com>
2024-12-03 07:11:11 +0200

    added getting input

--- advent_of_code_2024/day_3_pt_2_mul.c
+++ advent_of_code_2024/day_3_pt_2_mul.c
@@ -1,3 +1,16 @@
 /*
  * scan through looking for string "mul(" then check if both numers are good then multiply them
 */
+#include <stdio.h>
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+	}
+
+	free(input_line);
+	return 0;
+}
