60818a86b6f95148eb4dfd2e7bdd732e5bcad6f3
Emma Christy <emma.t.christy@gmail.com>
2024-12-04 18:58:14 +0200

    added hash table summing of nums

--- advent_of_code_2024/day_1_part_2_loc_diff_rust/src/main.rs
+++ advent_of_code_2024/day_1_part_2_loc_diff_rust/src/main.rs
@@ -1,7 +1,8 @@
 /*
- * parse input into vector of strings get sum of the absolute value of the differences between the first and second number
+ * parse input into vector of strings put both lists of nums in a hash map recording the number of occurrences when a val is found add the product of the val and the number of occurrences in the other array and add that to the product
 */
 use std::io;
+use std::collections::HashMap;
 
 fn main() {
     let mut input: Vec<String> = vec![String::new(); 1];
@@ -13,27 +14,31 @@ fn main() {
     }
     input.resize(line_count, String::new());//delets last empty line
 
-    let mut left_nums: Vec<i64> = vec![0; line_count];
-    let mut right_nums: Vec<i64> = vec![0; line_count];
+    let mut left_nums: HashMap<i64, i64>= HashMap::new();
+    let mut right_nums: HashMap<i64, i64>= HashMap::new();
+    let mut prod:i64 = 0;
 
     for line in input.iter().enumerate(){
-        for sub_str in line.1.split_whitespace().into_iter().enumerate(){
-            if sub_str.0 & 1 == 1{//im so bad at rust
-                right_nums[line.0] = sub_str.1.parse().unwrap();
-            }else{
-                left_nums[line.0] = sub_str.1.parse().unwrap();
-            }
+        let mut num_arr: [i64; 2] = [0, 0];
+        for sub_str in line.1.split_whitespace().into_iter().enumerate(){//slightly less bad way of doing this
+            num_arr[sub_str.0] = sub_str.1.parse().unwrap();
         }
+
+        left_nums.entry(num_arr[0]).or_insert(0);
+        let count = left_nums.get_key_value(&num_arr[0]).unwrap();
+        left_nums.insert(num_arr[0], count.1 + 1);
+
+        let zero = 0;
+        let count = right_nums.get_key_value(&num_arr[0]).unwrap_or((&zero, &zero));
+        prod += count.1 * num_arr[0];
+
+        right_nums.entry(num_arr[1]).or_insert(0);
+        let count = right_nums.get_key_value(&num_arr[1]).unwrap();
+        right_nums.insert(num_arr[1], count.1 + 1);
+
+        let count = left_nums.get_key_value(&num_arr[1]).unwrap_or((&zero, &zero));
+        prod += count.1 * num_arr[1];
     }
 
-    left_nums.sort();
-    right_nums.sort();
-
-    let mut dif_sum:i64 = 0;
-
-    for num in left_nums.iter().enumerate(){
-        dif_sum += (left_nums[num.0] - right_nums[num.0]).abs();
-    }
-
-    println!("{}", dif_sum);
+    println!("{}", prod);
 }
