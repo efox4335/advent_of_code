d8440d24472734dd54d8d1613cd31bb34435a4aa
Emma Christy <emma.t.christy@gmail.com>
2024-12-04 07:10:52 +0200

    added day 4 part 1 file

---
+++ advent_of_code_2024/day_4_pt_1_word_search.c
@@ -0,0 +1,18 @@
+/*
+ * get the input line by line and transform it so each way a match and happen is all in one line
+ * eg. diagonal matchs transformed this way make it look like a rhombus
+ * 	ab
+ * 	cd
+ * becomes
+ * 	a
+ * 	bc
+ * 	 d
+ * and
+ * 	 b
+ * 	ad
+ * 	c
+ * and
+ * 	bd
+ * 	ac
+ * have a fowards and backwards match so no mirror is required
+*/
