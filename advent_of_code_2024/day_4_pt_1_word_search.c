ea49c19aed06d5ece1259b8a8de6db9785b6d7cd
Emma Christy <emma.t.christy@gmail.com>
2024-12-04 07:20:41 +0200

    parse input into array

--- advent_of_code_2024/day_4_pt_1_word_search.c
+++ advent_of_code_2024/day_4_pt_1_word_search.c
@@ -16,3 +16,25 @@
  * 	ac
  * have a fowards and backwards match so no mirror is required
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	enum{BUFFER_LEN = 1000};
+
+	int line_count = 0;
+
+	char *input_line = NULL;
+	char input[BUFFER_LEN][BUFFER_LEN];
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+		strcpy(input[line_count], input_line);
+		++line_count;
+	}
+
+	free(input_line);
+	return 0;
+}
