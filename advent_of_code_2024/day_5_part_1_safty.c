41d1972005f8fb47035c0fbbe897bfeb1a757fda
Emma Christy <emma.t.christy@gmail.com>
2024-12-05 08:07:26 +0200

    added checking updates and summing mid page from good updates

--- advent_of_code_2024/day_5_part_1_safty.c
+++ advent_of_code_2024/day_5_part_1_safty.c
@@ -28,6 +28,18 @@ void rule_append(rule *rule_ptr, int rule_num)
 	++(rule_ptr->rule_count);
 }
 
+//returns one if update follows rule for num
+int check_compliance(rule *cur_rule, int *past_visited, int update_count)
+{
+	for(int i = 0; i < cur_rule->rule_count; ++i){
+		if(past_visited[cur_rule->rules[i]] == update_count){
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
 int main(void)
 {
 	enum{RULES, UPDATES};
@@ -44,9 +56,20 @@ int main(void)
 
 	int input_part = RULES;
 
+	char update_delim[] = ",";
+	int past_update_val_arr[100];
+	int update_count = 0;//if we use this to set the val in past_update_val_arr there is no need to 0 arr between updates
+
+	for(int i = 0; i < 100; ++i){
+		past_update_val_arr[i] = 0;
+	}
+
+	int mid_page_count = 0;
+
 	while(getline(&input_line, &lim, stdin) > 0){
 		if(input_line[0] == '\n'){
 			input_part = UPDATES;
+			continue;//skips past newline
 		}
 
 		if(input_part == RULES){
@@ -55,8 +78,33 @@ int main(void)
 
 			num_ptr = strtok(NULL, rule_delim);
 			rule_append(&rule_arr[right_rule], atoi(num_ptr));
+		}else{
+			int update_len = 0;
+			int update[100];
+			num_ptr = strtok(input_line, update_delim);
+			++update_count;
+
+			//because each page only appears once in each
+			while(num_ptr != NULL){
+				int cur_num = atoi(num_ptr);
+				if(!check_compliance(&rule_arr[cur_num], past_update_val_arr, update_count)){
+					break;
+				}
+				past_update_val_arr[cur_num] = update_count;
+				update[update_len] = cur_num;
+
+				++update_len;
+				num_ptr = strtok(NULL, update_delim);
+			}
+
+			//will be non null if check_compliance failed
+			if(num_ptr == NULL){
+				mid_page_count += update[((update_len - 1) / 2)];
+			}
 		}
 	}
+
+	printf("%d\n", mid_page_count);
 
 	free(input_line);
 	return 0;
