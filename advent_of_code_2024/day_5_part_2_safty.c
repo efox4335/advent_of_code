7446b71a1c93f0391727c472071142e528c2e612
Emma Christy <emma.t.christy@gmail.com>
2024-12-05 08:52:00 +0200

    added fixing and summing of broken updates

--- advent_of_code_2024/day_5_part_2_safty.c
+++ advent_of_code_2024/day_5_part_2_safty.c
@@ -3,7 +3,7 @@
  * when inputing the updates mark all numbers previously visited in an array by index and compare it with the
  * right values in the rule array
  * when a rule break is found swap with the first rule break
- * and recheck the array from that point in case the swap caused a rule break
+ * and recheck the array from the start in case the swap caused a rule break
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,16 +30,18 @@ void rule_append(rule *rule_ptr, int rule_num)
 	++(rule_ptr->rule_count);
 }
 
-//returns one if update follows rule for num
-int check_compliance(rule *cur_rule, int *past_visited, int update_count)
+//returns -1 for no error and the index of the earliest violation
+int check_compliance(rule *cur_rule, int past_visited[2][100], int update_count)
 {
+	int earliest_vol_index = 100;
+
 	for(int i = 0; i < cur_rule->rule_count; ++i){
-		if(past_visited[cur_rule->rules[i]] == update_count){
-			return 0;
+		if(past_visited[0][cur_rule->rules[i]] == update_count){
+			earliest_vol_index = (past_visited[1][cur_rule->rules[i]] < earliest_vol_index)? past_visited[1][cur_rule->rules[i]] : earliest_vol_index;
 		}
 	}
 
-	return 1;
+	return (earliest_vol_index == 100)? -1 : earliest_vol_index;
 }
 
 int main(void)
@@ -59,11 +61,11 @@ int main(void)
 	int input_part = RULES;
 
 	char update_delim[] = ",";
-	int past_update_val_arr[100];
+	int past_update_val_arr[2][100];//arr at past_update_val_arr[0][index] is if val at index has been encountered before arr at past_update_val_arr[1][index] is the index of that val in update_arr
 	int update_count = 0;//if we use this to set the val in past_update_val_arr there is no need to 0 arr between updates
 
 	for(int i = 0; i < 100; ++i){
-		past_update_val_arr[i] = 0;
+		past_update_val_arr[0][i] = 0;
 	}
 
 	int mid_page_count = 0;
@@ -84,23 +86,33 @@ int main(void)
 			int update_len = 0;
 			int update[100];
 			num_ptr = strtok(input_line, update_delim);
-			++update_count;
 
-			//because each page only appears once in each
 			while(num_ptr != NULL){
-				int cur_num = atoi(num_ptr);
-				if(!check_compliance(&rule_arr[cur_num], past_update_val_arr, update_count)){
-					break;
-				}
-				past_update_val_arr[cur_num] = update_count;
-				update[update_len] = cur_num;
+				update[update_len] = atoi(num_ptr);
 
 				++update_len;
 				num_ptr = strtok(NULL, update_delim);
 			}
 
+			++update_count;
+			int error_happened = 0;
+
+			for(int i = 0; i < update_len; ++i){
+				past_update_val_arr[0][update[i]] = update_count;
+				past_update_val_arr[1][update[i]] = i;
+
+				int earliest_vol_index = check_compliance(&rule_arr[update[i]], past_update_val_arr, update_count);
+				if(earliest_vol_index != -1){
+					error_happened = 1;
+					int temp = update[i];
+					update[i] = update[earliest_vol_index];
+					update[earliest_vol_index] = temp;
+					++update_count;
+					i = -1;
+				}
+			}
 			//will be non null if check_compliance failed
-			if(num_ptr == NULL){
+			if(error_happened){
 				mid_page_count += update[((update_len - 1) / 2)];
 			}
 		}
