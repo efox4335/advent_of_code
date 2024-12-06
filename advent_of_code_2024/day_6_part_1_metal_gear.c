8bb9be9a9cd451d2c52a17831200b72631425f20
Emma Christy <emma.t.christy@gmail.com>
2024-12-06 07:28:10 +0200

    added parsing of input into arr

--- advent_of_code_2024/day_6_part_1_metal_gear.c
+++ advent_of_code_2024/day_6_part_1_metal_gear.c
@@ -3,14 +3,21 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+enum{INPUT_BUFF_SIZE = 200};
 
 int main(void)
 {
 	char *input_line = NULL;
 	size_t lim = 0;
 
+	char input[INPUT_BUFF_SIZE][INPUT_BUFF_SIZE];
+	int line_count = 0;
+
 	while(getline(&input_line, &lim, stdin) > 1){
+		strcpy(input[line_count], input_line);
+		++line_count;
 	}
 
 	free(input_line);
