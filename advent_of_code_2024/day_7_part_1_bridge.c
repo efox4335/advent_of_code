3affe33ac3ba6a356926db077d18342380d36378
Emma Christy <emma.t.christy@gmail.com>
2024-12-07 08:59:21 +0200

    added checking for good values

--- advent_of_code_2024/day_7_part_1_bridge.c
+++ advent_of_code_2024/day_7_part_1_bridge.c
@@ -8,6 +8,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+//returns 1 if the total is possible from num_arr
+//when any number that could be a factor also check if it could be added to get the total
+int is_possible(size_t total, size_t *num_arr, int num_count)
+{
+	long long int temp_total = total;
+
+	for(int i = num_count - 1; i >= 1; --i){
+		if(temp_total <= 0){
+			break;
+		}else if((temp_total % num_arr[i]) == 0ll){
+			if(is_possible(temp_total - num_arr[i], num_arr, i)){
+				return 1;
+			}
+			temp_total /= num_arr[i];
+		}else{
+			temp_total -= num_arr[i];
+		}
+	}
+
+	//will always fail if temp_total <= 0 as all numbers are > 0
+	if(temp_total == num_arr[0]){
+		return 1;
+	}
+
+	return 0;
+}
+
 int main(void)
 {
 	char *input_line = NULL;
@@ -32,7 +59,13 @@ int main(void)
 			++num_count;
 			temp_num = strtok(NULL, delim);
 		}
+
+		if(is_possible(total, input, num_count)){
+			good_total += total;
+		}
 	}
+
+	printf("%lu\n", good_total);
 
 	free(input_line);
 	return 0;
