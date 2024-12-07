e9c338517840963465f7857c34b6ca5df8b6ffba
Emma Christy <emma.t.christy@gmail.com>
2024-12-07 07:41:54 +0200

    added getting input into array

--- advent_of_code_2024/day_7_part_1_bridge.c
+++ advent_of_code_2024/day_7_part_1_bridge.c
@@ -6,13 +6,32 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
 	char *input_line = NULL;
 	size_t lim = 0;
 
+	size_t total = 0;
+	size_t input[100];
+	int num_count = 0;
+	size_t good_total = 0;
+
+	char delim[] = ": \n";
+	char *temp_num = NULL;
+
 	while(getline(&input_line, &lim, stdin) > 1){
+		temp_num = strtok(input_line, delim);
+		total = atol(temp_num);
+
+		num_count = 0;
+		temp_num = strtok(NULL, delim);
+		while(temp_num != NULL){
+			input[num_count] = atol(temp_num);
+			++num_count;
+			temp_num = strtok(NULL, delim);
+		}
 	}
 
 	free(input_line);
