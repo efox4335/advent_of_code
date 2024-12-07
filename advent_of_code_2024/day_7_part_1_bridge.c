13713290ddede107f70b3a24c5f63250cdcc5346
Emma Christy <emma.t.christy@gmail.com>
2024-12-07 07:25:23 +0200

    added day 7 part 1 file

---
+++ advent_of_code_2024/day_7_part_1_bridge.c
@@ -0,0 +1,6 @@
+/*
+ * for the last number in the list it can only be multiplied by a large number or add to a large number
+ * the only way for it to multiply a large number is for it to be a factor of the total
+ * if not the only way the total can be correct is if the last operation is '+'
+ * do this for the next last number with the total being '-' or '/' depending on the operation before it
+*/
