76c1c9ce324d920a5c564adcac8f55a2e97f8a0b
Emma Christy <emma.t.christy@gmail.com>
2024-12-08 06:52:06 +0200

    added util to get input line by line from stdin

---
+++ utils/get_input_by_line_c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *input_line = NULL;
+    size_t lim = 0;
+
+    while(getline(&input_line, &lim, stdin) > 1){
+    }
+
+    free(input_line);
+    return 0;
+}
