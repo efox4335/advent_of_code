9411ab942edb512bc1d21a1cd6957e632bf691d7
Emma Christy <emma.t.christy@gmail.com>
2024-12-08 07:28:59 +0200

    added getting antenna pos and count

--- advent_of_code_2024/day_8_part_1_antinode.c
+++ advent_of_code_2024/day_8_part_1_antinode.c
@@ -6,13 +6,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+	int row;
+	int col;
+}cords;
+
 int main(void)
 {
+	//index by ascii num
+	int antenna_type_count[256];
+
+	for(int i = 0; i < 256; ++i){
+		antenna_type_count[i] = 0;
+	}
+
+	cords antenna_pos_arr[256][100];
+	int line_count = 0;
+
 	char *input_line = NULL;
 	size_t lim = 0;
 
 	while(getline(&input_line, &lim, stdin) > 1){
-		printf("%s", input_line);
+		for(int i = 0; input_line[i] != '\0'; ++i){
+			if(input_line[i] == '.' || input_line[i] == '\n'){
+				continue;
+			}
+			antenna_pos_arr[(int) input_line[i]][antenna_type_count[(int) input_line[i]]].row = line_count;
+			antenna_pos_arr[(int) input_line[i]][antenna_type_count[(int) input_line[i]]].col = i;
+
+			antenna_type_count[(int) input_line[i]] += 1;
+		}
+		++line_count;
 	}
 
 	free(input_line);
