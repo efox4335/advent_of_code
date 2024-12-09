0b21b894c952557b06345f12332df3873eb32993
Emma Christy <emma.t.christy@gmail.com>
2024-12-09 08:43:31 +0200

    added moving files and getting checksum

--- advent_of_code_2024/day_9_part_2_data_loss.c
+++ advent_of_code_2024/day_9_part_2_data_loss.c
@@ -4,6 +4,7 @@
  * have the for each id have start_index look from the start for an empty space block big enough
  * and keep the checksum accurate
  * keeping track of empty block size and position can be done by adding the new moved data size to the file block before the empty block and subtracting it from the empty block
+ * keeping track of original file size to avoid counting later files addition to the block
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,9 +15,14 @@ int final_id(int block_count)
 	return block_count / 2;
 }
 
+typedef struct{
+	size_t f_size;//size of file that was originally part of that block
+	size_t b_size;
+}file;
+
 int main(void)
 {
-	int block_arr[20000];
+	file block_arr[20000];
 	int block_count = 0;
 
 	char *input_line = NULL;
@@ -26,52 +32,51 @@ int main(void)
 	}
 
 	for(int i = 0; input_line[i] != '\n'; ++i){
-		block_arr[i] = input_line[i] - '0';
+		block_arr[i].f_size = input_line[i] - '0';
+		block_arr[i].b_size = input_line[i] - '0';
 		++block_count;
 	}
 
 	size_t cur_fs_pos = 0;
 	size_t st_index = 0;
 	size_t end_index = (block_count & 1)? block_count - 1: block_count - 2;//skips last empty block if there is one
-	size_t st_id = 0;
 	size_t end_id = final_id(block_count);
 	size_t checksum = 0;
 
-	while(st_index <= end_index){
-		if(st_index & 1){
-			if(block_arr[end_index] == 0){
-				end_index -= 2;
-				--end_id;
-				if(st_index >= end_index){//if st_index is at the last empty block and the last file block is empty to avoid counting anything twice
-					break;
-				}
-			}
-
-			while(block_arr[st_index] > 0){
-				if(block_arr[end_index] == 0){
-					end_index -= 2;
-					--end_id;
-					if(st_index >= end_index){//if st_index is at the last empty block and the last file block is empty to avoid counting anything twice
-						break;
+	while(end_id > 0){
+		st_index = 0;
+		cur_fs_pos = 0;
+		while(st_index < end_index){
+			if(st_index & 1){
+				if(block_arr[st_index].b_size >= block_arr[end_index].f_size){
+					while(block_arr[end_index].f_size > 0){
+						block_arr[st_index - 1].b_size += 1;
+						block_arr[st_index].b_size -= 1;
+						block_arr[end_index].f_size -= 1;
+						checksum += end_id * cur_fs_pos;
+						++cur_fs_pos;
 					}
-				}
 
-				block_arr[end_index] -= 1;
-				block_arr[st_index] -= 1;
+					break;
+				}else{
+					cur_fs_pos += block_arr[st_index].b_size;
+				}
+			}else{
+				cur_fs_pos += block_arr[st_index].b_size;
+			}
+			++st_index;
+		}
+
+		if(st_index == end_index){
+			while(block_arr[end_index].f_size > 0){
+				block_arr[end_index].f_size -= 1;
 				checksum += end_id * cur_fs_pos;
 				++cur_fs_pos;
 			}
-
-		}else{
-			for(int i = 0; i < block_arr[st_index]; ++i){
-				checksum += st_id * cur_fs_pos;
-				++cur_fs_pos;
-			}
-
-			++st_id;
 		}
 
-		++st_index;
+		end_index -= 2;
+		--end_id;
 	}
 
 	printf("%lu\n", checksum);
