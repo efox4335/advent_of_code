41546c55dd7498cabbe7e50464827c62ed55b779
Emma Christy <emma.t.christy@gmail.com>
2024-12-09 07:36:02 +0200

    added day 9 part 1 file

---
+++ advent_of_code_2024/day_9_part_1_data_loss.c
@@ -0,0 +1,23 @@
+/*
+ * when taking in data keep track of the max id
+ * have a 2 index sweep of the file system one from the start and one from the end
+ * when the start one encounters a file block it's id * index shall be added to a total
+ * when the start one encounters an empty space block the index at the end shall find the first file block
+ * when the file block is found it will simulate a move to fill the empty space and add the id * index to the total
+ * if the file block at the end is empty and there is more empty space the index at the end shall find the next file block
+ * if the empty block is filled before the file block is emptyed then the start index will continue
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+	}
+
+	free(input_line);
+	return 0;
+}
