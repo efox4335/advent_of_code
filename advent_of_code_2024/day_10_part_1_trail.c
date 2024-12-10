79fad4cdfc21d286a3490286629fd0e25bcd4b47
Emma Christy <emma.t.christy@gmail.com>
2024-12-10 07:23:13 +0200

    added parsing of input into arr of ints

--- advent_of_code_2024/day_10_part_1_trail.c
+++ advent_of_code_2024/day_10_part_1_trail.c
@@ -6,10 +6,19 @@
 
 int main(void)
 {
+	enum{BUFF_SIZE = 100};
+
+	int paths[BUFF_SIZE][BUFF_SIZE];
+	int line_count = 0;
+
 	char *input_line = NULL;
 	size_t lim = 0;
 
 	while(getline(&input_line, &lim, stdin) > 1){
+		for(int i = 0; input_line[i] != '\n'; ++i){
+			paths[line_count][i] = input_line[i] - '0';
+		}
+		++line_count;
 	}
 
 	free(input_line);
