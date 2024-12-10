4fd378c160d50e2bbca502c93f25007bd06b3f8f
Emma Christy <emma.t.christy@gmail.com>
2024-12-10 07:17:51 +0200

    added day 10 part 1 file

---
+++ advent_of_code_2024/day_10_part_1_trail.c
@@ -0,0 +1,17 @@
+/*
+ * a recursive memoized function will walk the path and return the number of trail ends connected to a path
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+	}
+
+	free(input_line);
+	return 0;
+}
