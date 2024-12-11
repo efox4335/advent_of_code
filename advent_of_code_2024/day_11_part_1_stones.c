614149904e185ba71b6df5630aed889acf2520fd
Emma Christy <emma.t.christy@gmail.com>
2024-12-11 07:40:45 +0200

    added parsing of input into file

--- advent_of_code_2024/day_11_part_1_stones.c
+++ advent_of_code_2024/day_11_part_1_stones.c
@@ -4,13 +4,27 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
 	char *input_line = NULL;
 	size_t lim = 0;
 
+	long input_arr[10];
+	int num_count = 0;
+	char delim[] = " \n";
+
 	while(getline(&input_line, &lim, stdin) > 1){
+		char *temp_num = NULL;
+
+		temp_num = strtok(input_line, delim);
+
+		while(temp_num != NULL){
+			input_arr[num_count] = atol(temp_num);
+			++num_count;
+			temp_num = strtok(NULL, delim);
+		}
 	}
 
 	free(input_line);
