0faf38cad4c59eed91df8976562d0ab222f8c842
Emma Christy <emma.t.christy@gmail.com>
2024-12-12 07:55:39 +0200

    added parsing of input into 2d array

--- advent_of_code_2024/day_12_part_1_fence.c
+++ advent_of_code_2024/day_12_part_1_fence.c
@@ -2,16 +2,34 @@
  * for a polygon made of squares where you have points in the center of each edge
  * the area is equal to the shoelace theorm + 1/2 perimeter - 1
  * which is picks theorm
+ * when processing each regon each plant within it will be marked to avoid processing it twice
 */
 #include <stdio.h>
 #include <stdlib.h>
 
+enum{VISITED, NOT_VISITED};
+enum{BUF_SIZE = 200};
+
+typedef struct{
+	char plant;
+	int visited;
+}plant;
+
 int main(void)
 {
+	int line_count = 0;
+	plant input[BUF_SIZE][BUF_SIZE];
+
 	char *input_line = NULL;
 	size_t lim = 0;
 
 	while(getline(&input_line, &lim, stdin) > 1){
+		for(int i = 0; input_line[i] != '\n'; ++i){
+			input[line_count][i].plant = input_line[i];
+			input[line_count][i].visited = NOT_VISITED;
+		}
+
+		++line_count;
 	}
 
 	free(input_line);
