17d50f77ae05644333d2aa4fe2b0433727cf4177
Emma Christy <emma.t.christy@gmail.com>
2024-12-12 13:20:00 +0200

    added calculating area with holes

--- advent_of_code_2024/day_12_part_1_fence.c
+++ advent_of_code_2024/day_12_part_1_fence.c
@@ -1,8 +1,5 @@
 /*
- * for a polygon made of squares where you have points in the center of each edge
- * the area is equal to the shoelace theorm + 1/2 perimeter - 1
- * which is picks theorm
- * when processing each regon each plant within it will be marked to avoid processing it twice
+ * gets area via flood fill
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,6 +7,7 @@
 
 enum{VISITED, NOT_VISITED};
 enum{BUF_SIZE = 200};
+enum{TOP, BOTTOM, LEFT, RIGHT};
 
 typedef struct{
 	char plant;
@@ -21,58 +19,33 @@ typedef struct{
 	int col;
 }cord;
 
+typedef struct{
+	int perimeter;
+	int area;
+}shape;
+
+typedef struct{
+	cord loc;
+	int dir;
+}plant_dir;
+
 //starts from point
 //returns perimeter and sets polygon to a list of points along the edges including inner corners
 //simulates walking perimeter by keeping track of each plant + edge if a plant + edge has already been seen the perimeter is found
 //stores all plant + edge combinations in a hash table
 //explores clockwise
-int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE][BUF_SIZE], const int line_count)
+//perim_cashe keeps track of all direction+location so no double counting happens
+int get_perimeter(const cord point, const int direction, const plant input[BUF_SIZE][BUF_SIZE], const int line_count, edsa_htable *perim_cashe)
 {
-	enum{TOP, BOTTOM, LEFT, RIGHT};
-
-	typedef struct{
-		cord loc;
-		int dir;
-	}plant_dir;
-
 	int perimeter = 0;
-	int polygon_index = 0;
-
-	edsa_htable *seen_before = NULL;
-	edsa_htable_init(&seen_before, sizeof(plant_dir), sizeof(int), 100);
-
-	int cur_edge_dir;
-	if(point.row == 0 || input[point.row][point.col].plant != input[point.row - 1][point.col].plant){
-		cur_edge_dir = TOP;
-	}else if(point.row == line_count - 1 || input[point.row][point.col].plant != input[point.row + 1][point.col].plant){
-		cur_edge_dir = BOTTOM;
-	}else if(point.col == 0 || input[point.row][point.col].plant != input[point.row][point.col - 1].plant){
-		cur_edge_dir = LEFT;
-	}else if(point.col == line_count - 1 || input[point.row][point.col].plant != input[point.row][point.col + 1].plant){
-		cur_edge_dir = RIGHT;
-	}else{
-		printf("error passed non edge point %d %d\n", point.row, point.col);
-		exit(1);
-	}
-
 	int temp = 1;
 	plant_dir seen_key;
 	seen_key.loc.row = point.row;
 	seen_key.loc.col = point.col;
-	seen_key.dir = cur_edge_dir;
+	seen_key.dir = direction;
 
-	do{
-		edsa_htable_ins(seen_before, &seen_key, &temp);
-		input[seen_key.loc.row][seen_key.loc.col].visited = VISITED;
-
-		polygon[polygon_index].row = seen_key.loc.row;
-		polygon[polygon_index].col = seen_key.loc.col;
-
-		++polygon_index;
-
-		polygon[polygon_index].row = -1;
-		polygon[polygon_index].col = -1;
-
+	while(edsa_htable_read(perim_cashe, &seen_key, &temp) != EDSA_SUCCESS){
+		edsa_htable_ins(perim_cashe, &seen_key, &temp);
 		plant_dir next_plant;
 
 		switch(seen_key.dir){
@@ -83,8 +56,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.dir = RIGHT;
 				next_plant.loc.row = seen_key.loc.row;
 				next_plant.loc.col = seen_key.loc.col;
-
-				--polygon_index;//avoids double counting the same square
 			//line
 			}else if(seen_key.loc.row == 0 ||
 			input[seen_key.loc.row][seen_key.loc.col].plant != input[seen_key.loc.row - 1][seen_key.loc.col + 1].plant){//no need to check neighbor as we already know it matches
@@ -93,11 +64,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.loc.col = seen_key.loc.col + 1;
 			//inner corner
 			}else{
-				polygon[polygon_index].row = seen_key.loc.row;
-				polygon[polygon_index].col = seen_key.loc.col + 1;
-
-				++polygon_index;
-
 				next_plant.dir = LEFT;
 				next_plant.loc.row = seen_key.loc.row - 1;
 				next_plant.loc.col = seen_key.loc.col + 1;
@@ -110,8 +76,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.dir = LEFT;
 				next_plant.loc.row = seen_key.loc.row;
 				next_plant.loc.col = seen_key.loc.col;
-
-				--polygon_index;//avoids double counting the same square
 			//line
 			}else if(seen_key.loc.row == line_count - 1 ||
 			input[seen_key.loc.row][seen_key.loc.col].plant != input[seen_key.loc.row + 1][seen_key.loc.col - 1].plant){//no need to check neighbor as we already know it matches
@@ -120,11 +84,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.loc.col = seen_key.loc.col - 1;
 			//inner corner
 			}else{
-				polygon[polygon_index].row = seen_key.loc.row;
-				polygon[polygon_index].col = seen_key.loc.col - 1;
-
-				++polygon_index;
-
 				next_plant.dir = RIGHT;
 				next_plant.loc.row = seen_key.loc.row + 1;
 				next_plant.loc.col = seen_key.loc.col - 1;
@@ -137,8 +96,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.dir = TOP;
 				next_plant.loc.row = seen_key.loc.row;
 				next_plant.loc.col = seen_key.loc.col;
-
-				--polygon_index;//avoids double counting the same square
 			//line
 			}else if(seen_key.loc.col == 0 ||
 			input[seen_key.loc.row][seen_key.loc.col].plant != input[seen_key.loc.row - 1][seen_key.loc.col - 1].plant){//no need to check neighbor as we already know it matches
@@ -147,11 +104,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.loc.col = seen_key.loc.col;
 			//inner corner
 			}else{
-				polygon[polygon_index].row = seen_key.loc.row - 1;
-				polygon[polygon_index].col = seen_key.loc.col;
-
-				++polygon_index;
-
 				next_plant.dir = BOTTOM;
 				next_plant.loc.row = seen_key.loc.row - 1;
 				next_plant.loc.col = seen_key.loc.col - 1;
@@ -164,8 +116,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.dir = BOTTOM;
 				next_plant.loc.row = seen_key.loc.row;
 				next_plant.loc.col = seen_key.loc.col;
-
-				--polygon_index;//avoids double counting the same square
 			//line
 			}else if(seen_key.loc.col == line_count - 1 ||
 			input[seen_key.loc.row][seen_key.loc.col].plant != input[seen_key.loc.row + 1][seen_key.loc.col + 1].plant){//no need to check neighbor as we already know it matches
@@ -174,11 +124,6 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 				next_plant.loc.col = seen_key.loc.col;
 			//inner corner
 			}else{
-				polygon[polygon_index].row = seen_key.loc.row + 1;
-				polygon[polygon_index].col = seen_key.loc.col;
-
-				++polygon_index;
-
 				next_plant.dir = TOP;
 				next_plant.loc.row = seen_key.loc.row + 1;
 				next_plant.loc.col = seen_key.loc.col + 1;
@@ -186,32 +131,75 @@ int get_perimeter(const cord point, cord *restrict polygon, plant input[BUF_SIZE
 			break;
 		}
 
-		++perimeter;
 		seen_key.loc.row = next_plant.loc.row;
 		seen_key.loc.col = next_plant.loc.col;
 		seen_key.dir = next_plant.dir;
-	}while(edsa_htable_read(seen_before, &seen_key, &temp) != EDSA_SUCCESS);
 
-	edsa_htable_free(seen_before);
+		++perimeter;
+	}
+
+
 	return perimeter;
 }
 
-//-1 acts as the sentinal value
-//returns the area from the shorlace fromula
-int shoelace(cord *polygon)
+//gets perimeter and area
+//gets area by floodfill
+shape get_shape_data(cord point, plant input[BUF_SIZE][BUF_SIZE], int line_count)
 {
-	int area = 0;
-	int index = 0;
+	shape shape_data;
+	shape_data.area = 0;
+	shape_data.perimeter = 0;
+	cord visit_stack[10000];
+	int visit_stack_ind = 0;
 
-	for(index = 0; polygon[index + 1].row != -1; ++index){
-		area += (polygon[index].row * polygon[index + 1].col) - (polygon[index].col * polygon[index + 1].row);
+	edsa_htable *perim_cashe = NULL;
+	edsa_htable_init(&perim_cashe, sizeof(plant_dir), sizeof(int), 100);
+
+	visit_stack[visit_stack_ind] = point;
+	++visit_stack_ind;
+
+	while(visit_stack_ind != 0){
+		--visit_stack_ind;
+		point = visit_stack[visit_stack_ind];
+		if(input[point.row][point.col].visited == VISITED){
+			continue;
+		}
+		input[point.row][point.col].visited = VISITED;
+
+		if(point.row == 0 || input[point.row][point.col].plant != input[point.row - 1][point.col].plant){
+			shape_data.perimeter += get_perimeter(point, TOP, input, line_count, perim_cashe);
+		}else if(input[point.row - 1][point.col].visited != VISITED){
+			visit_stack[visit_stack_ind].row = point.row - 1;
+			visit_stack[visit_stack_ind].col = point.col;
+			++visit_stack_ind;
+		}
+		if(point.row == line_count - 1 || input[point.row][point.col].plant != input[point.row + 1][point.col].plant){
+			shape_data.perimeter += get_perimeter(point, BOTTOM, input, line_count, perim_cashe);
+		}else if(input[point.row + 1][point.col].visited != VISITED){
+			visit_stack[visit_stack_ind].row = point.row + 1;
+			visit_stack[visit_stack_ind].col = point.col;
+			++visit_stack_ind;
+		}
+		if(point.col == 0 || input[point.row][point.col].plant != input[point.row][point.col - 1].plant){
+			shape_data.perimeter += get_perimeter(point, LEFT, input, line_count, perim_cashe);
+		}else if(input[point.row][point.col - 1].visited != VISITED){
+			visit_stack[visit_stack_ind].row = point.row;
+			visit_stack[visit_stack_ind].col = point.col - 1;
+			++visit_stack_ind;
+		}
+		if(point.col == line_count - 1 || input[point.row][point.col].plant != input[point.row][point.col + 1].plant){
+			shape_data.perimeter += get_perimeter(point, RIGHT, input, line_count, perim_cashe);
+		}else if(input[point.row][point.col + 1].visited != VISITED){
+			visit_stack[visit_stack_ind].row = point.row;
+			visit_stack[visit_stack_ind].col = point.col + 1;
+			++visit_stack_ind;
+		}
+
+		shape_data.area += 1;
 	}
 
-	area += (polygon[index].row * polygon[0].col) - (polygon[index].col * polygon[0].row);
-
-	area = abs(area);
-
-	return area >> 1;
+	edsa_htable_free(perim_cashe);
+	return shape_data;
 }
 
 int main(void)
@@ -231,27 +219,19 @@ int main(void)
 		++line_count;
 	}
 
-	char type = '\n';//used to skip past already seen types
 	int total_price = 0;
 
 	for(int i = 0; i < line_count; ++i){
 		for(int j = 0; j < line_count; ++j){
-			if(type == input[i][j].plant || input[i][j].visited == VISITED){
-				type = input[i][j].plant;
-				continue;
+			if(input[i][j].visited == NOT_VISITED){
+				cord point;
+				point.row = i;
+				point.col = j;
+				shape shape_data = get_shape_data(point, input, line_count);
+
+				total_price += (shape_data.area * shape_data.perimeter);
 			}
-			cord polygon[BUF_SIZE];
-			cord point;
-			point.row = i;
-			point.col = j;
-
-			int perimeter = get_perimeter(point, polygon, input, line_count);
-			total_price += (shoelace(polygon) + (perimeter / 2) - 1) * perimeter;
-
-			type = input[i][j].plant;
 		}
-
-		type = '\n';//to avoid excluding where a line ends with the same type of plant another line ends with
 	}
 
 	printf("%d\n", total_price);
