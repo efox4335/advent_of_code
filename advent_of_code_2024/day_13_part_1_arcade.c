505c5cf46106542ba394f9c3f4a5ab74a047aaaf
Emma Christy <emma.t.christy@gmail.com>
2024-12-13 10:03:18 +0200

    refactored to calculate button presses in a less error prone way

--- advent_of_code_2024/day_13_part_1_arcade.c
+++ advent_of_code_2024/day_13_part_1_arcade.c
@@ -1,40 +1,37 @@
 /*
- * for 2 non parallel lines there can be only one intersection point
- * set one line at the origin and one at the claw pos then do a gaussian elimination to see where they intersect
- * equations will be in form k = dy * x - dx * y
- * k can be calculated by filling in x and y for the point they intersect
- * the gaussian elimination will need to deal with non integar numbers but rounding to the nearist whole number should take care of errors
- * the number of times each button was pressed for k = 0 is intersect x / dx
- * and for k != 0 is (claw_end_point - intersect x) / dx
+ * button press count is the intersection of
+ * prizex = a_count * adx + b_count * bdx
+ * prizey = a_count * ady + b_count * bdy
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 //double prescision will represent the number of digits correctly
 typedef struct{
-	double dx1;
-	double dy1;
-	double dx2;
-	double dy2;
-	double k1;
-	double k2;
+	double adx;
+	double ady;
+	double bdx;
+	double bdy;
+	double prizex;
+	double prizey;
 }equation;
 
 typedef struct{
-	double x;
-	double y;
-}cord;
+	double a_count;
+	double b_count;
+}press_count;
 
-//returns intersection point of these 2 equations
-cord gaussian(const equation cur_equ)
+//returns button press count
+press_count gaussian(const equation cur_equ)
 {
-	const double row_dif = (cur_equ.dy2 / cur_equ.dy1);
-	cord intersect_point;
-	intersect_point.y = cur_equ.k2 / ((-1 * cur_equ.dx2) - ((-1 * cur_equ.dx1) * row_dif));
-	intersect_point.x = (cur_equ.dx1 * intersect_point.y) / cur_equ.dy1;//(dx1intersect_point.y)/dy1 = x_intersect
+	const double row_dif = (cur_equ.ady / cur_equ.adx);
+	press_count cur_count;
+	cur_count.b_count = (cur_equ.prizey - (cur_equ.prizex * row_dif)) / (cur_equ.bdy - (row_dif * cur_equ.bdx));
+	cur_count.a_count = (cur_equ.prizex - (cur_count.b_count * cur_equ.bdx)) / cur_equ.adx;
 
-	return intersect_point;
+	return cur_count;
 }
 
 int main(void)
@@ -43,13 +40,14 @@ int main(void)
 	size_t lim = 0;
 
 	equation cur_equ;
-	cord prize_pos;
 
 	char delim[] = "ButonPrizeA: XY+=\n,";
 	char *num_ptr = NULL;
 
 	enum{ENTER, EQU_1, EQU_2, PRIZE};
 	int part = ENTER;
+
+	long token_count = 0;
 
 	while(getline(&input_line, &lim, stdin) > 0){
 		if(input_line[0] == '\n'){
@@ -60,48 +58,43 @@ int main(void)
 		case ENTER:
 		case EQU_1:
 			num_ptr = strtok(input_line, delim);
-			cur_equ.dx1 = atof(num_ptr);
+			cur_equ.adx = atof(num_ptr);
 
 			num_ptr = strtok(NULL, delim);
-			cur_equ.dy1 = atof(num_ptr);
+			cur_equ.ady = atof(num_ptr);
 
 			part = EQU_2;
 			break;
 		case EQU_2:
 			num_ptr = strtok(input_line, delim);
-			cur_equ.dx2 = atof(num_ptr);
+			cur_equ.bdx = atof(num_ptr);
 
 			num_ptr = strtok(NULL, delim);
-			cur_equ.dy2 = atof(num_ptr);
+			cur_equ.bdy = atof(num_ptr);
 
 			part = PRIZE;
 			break;
 		case PRIZE:
 			num_ptr = strtok(input_line, delim);
-			prize_pos.x = atof(num_ptr);
+			cur_equ.prizex = atof(num_ptr);
 
 			num_ptr = strtok(NULL, delim);
-			prize_pos.y = atof(num_ptr);
+			cur_equ.prizey = atof(num_ptr);
 
 			part = EQU_1;
 			break;
 		}
 
 		if(part == EQU_1){
-			cur_equ.k1 = 0;
-			cur_equ.k2 = (prize_pos.x * cur_equ.dy2) - (prize_pos.y * cur_equ.dx2);
+			press_count cur_button = gaussian(cur_equ);
 
-			cord inter_point = gaussian(cur_equ);
-
-			//interscetion point makes it impossaible to get the prize
-			if(inter_point.x > prize_pos.x || inter_point.x < 0 || inter_point.y > prize_pos.y || inter_point.y < 0){
-				continue;
+			if(cur_button.a_count > 0 && cur_button.b_count > 0 && fabs(cur_button.a_count - round(cur_button.a_count)) < 0.01 && fabs(cur_button.b_count - round(cur_button.b_count)) < 0.01){
+				token_count += 3 * cur_button.a_count + cur_button.b_count;
 			}
-
-			long a_press_count = inter_point.x / cur_equ.dx1;
-			long b_press_count = (prize_pos.x - inter_point.x) / cur_equ.dx2;
 		}
 	}
+
+	printf("%ld\n", token_count);
 
 	free(input_line);
 	return 0;
