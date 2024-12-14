6a608852a1b834803e452787c0b8c468b00a172c
Emma Christy <emma.t.christy@gmail.com>
2024-12-14 07:21:04 +0200

    added day 14 part 1 file

---
+++ advent_of_code_2024/day_14_part_1_bathroom.c
@@ -0,0 +1,17 @@
+/*
+ * just a simple cycle detection
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+	}
+
+	free(input_line);
+	return 0;
+}
