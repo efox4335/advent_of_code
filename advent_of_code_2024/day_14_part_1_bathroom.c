5d42df4b2c5f47ff80b60e66bd1d241b15a07519
Emma Christy <emma.t.christy@gmail.com>
2024-12-14 07:30:31 +0200

    added parsing of file

--- advent_of_code_2024/day_14_part_1_bathroom.c
+++ advent_of_code_2024/day_14_part_1_bathroom.c
@@ -3,13 +3,40 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+	long row;
+	long col;
+}cord;
+
+typedef struct{
+	cord st_point;
+	cord vel;
+}robot;
 
 int main(void)
 {
+	char delim[] = "pv=,\n";
+	char *int_ptr = NULL;
+
 	char *input_line = NULL;
 	size_t lim = 0;
 
+	robot cur_rb;
+
 	while(getline(&input_line, &lim, stdin) > 1){
+		int_ptr = strtok(input_line, delim);
+		cur_rb.st_point.col = atol(int_ptr);
+
+		int_ptr = strtok(NULL, delim);
+		cur_rb.st_point.row = atol(int_ptr);
+
+		int_ptr = strtok(NULL, delim);
+		cur_rb.vel.col = atol(int_ptr);
+
+		int_ptr = strtok(NULL, delim);
+		cur_rb.vel.row = atol(int_ptr);
 	}
 
 	free(input_line);
