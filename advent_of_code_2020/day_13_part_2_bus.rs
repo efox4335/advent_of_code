7137c8b0af8a87d310c92ff935019701c58ad5b3
Emma Christy <emma.t.christy@gmail.com>
2024-12-14 18:17:53 +0200

    added crt to get the numbers

--- advent_of_code_2020/day_13_part_2_bus.rs
+++ advent_of_code_2020/day_13_part_2_bus.rs
@@ -1,5 +1,8 @@
+//no euclid algo needed as the numbers are small
+
 use std::io::stdin;
 
+#[derive(Clone, Copy)]
 struct BusTimes {
     id: u64,
     arival_time: u64,
@@ -19,10 +22,31 @@ fn main() {
         .enumerate()
     {
         if !id.is_empty() && id.as_bytes()[0] != b'x' {
+            let id_val = id.parse::<u64>().unwrap();
             bus_vec.push(BusTimes {
-                id: id.parse().unwrap(),
-                arival_time: num as u64,
+                id: id_val,
+                arival_time: (id_val - (num as u64 % id_val)) % id_val,
             });
         }
     }
+
+    let mut prod = 1;
+    for i in bus_vec.iter(){
+        prod *= i.id;
+    }
+
+    let mut time: u128 = 0;
+    for id in bus_vec.iter(){
+        let base_val = prod / id.id;
+        let mut add_val = base_val;
+
+        while add_val % id.id != id.arival_time{
+            add_val %= prod;
+            add_val += base_val;
+        }
+
+        time += add_val as u128;
+        time %= prod as u128;
+    }
+    println!("{}", time % prod as u128);
 }
