dfc8681c8aa61e5de36168b0832b355dd4f06bc1
Emma Christy <emma.t.christy@gmail.com>
2024-12-14 14:32:20 +0200

    added getting the shortest wait time

--- advent_of_code_2020/day_13_part_1_bus.rs
+++ advent_of_code_2020/day_13_part_1_bus.rs
@@ -8,7 +8,6 @@ fn main() {
 
     line.clear();
 
-
     let _ = stdin().read_line(&mut line);
 
     let bus_id: Vec<u32> = line
@@ -16,4 +15,18 @@ fn main() {
         .filter(|&id| !id.is_empty())
         .map(|id| id.parse().unwrap())
         .collect();
+
+    let mut soonest_bus_id = u32::MAX;
+    let mut wait = u32::MAX;
+    for id in bus_id{
+        let id_mod = ear_leave % id;
+        let cur_wait = id - id_mod;
+
+        if cur_wait < wait{
+            soonest_bus_id = id;
+            wait = cur_wait;
+        }
+    }
+
+    println!("{}", soonest_bus_id * wait);
 }
