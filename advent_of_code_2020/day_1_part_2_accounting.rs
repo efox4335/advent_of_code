aac1b748f5bffefa38f8b1808ccc13e529164576
Emma Christy <emma.t.christy@gmail.com>
2024-12-15 17:26:49 +0200

    solved day 1 part 2

--- advent_of_code_2020/day_1_part_2_accounting.rs
+++ advent_of_code_2020/day_1_part_2_accounting.rs
@@ -1,6 +1,9 @@
 /*
  * hash table where the key is the diff between 2020 and num
+ * create a secondary hash table with all the pairs of differences between that one and the input list
  * record is num
+ *
+ * should be O(n^2)
 */
 
 use std::{collections::HashMap, io::stdin};
@@ -16,10 +19,18 @@ fn main() {
         map.insert(2020 - num, *num);
     }
 
+    let mut second_order_map = HashMap::<i32, i32>::new();
+    for val in map.iter(){
+        for num in input.iter(){
+            second_order_map.insert(val.0 - num, *num);
+        }
+    }
+
     for num in input {
-        match map.get(&num) {
+        match second_order_map.get(&num) {
             Some(val) => {
-                println!("{}", val * num);
+                let temp = val + num;
+                println!("{}", val * num * map.get(&temp).unwrap());
                 std::process::exit(0);
             }
             _ => (),
