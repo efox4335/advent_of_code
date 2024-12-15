d8ee1cc99ecd1a2a906a9bd6a9c475f4c8f4d28d
Emma Christy <emma.t.christy@gmail.com>
2024-12-15 07:27:43 +0200

    added parsing of input

--- advent_of_code_2024/day_15_part_1_warehouse.c
+++ advent_of_code_2024/day_15_part_1_warehouse.c
@@ -5,12 +5,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum{BUF_SIZE = 200};
+enum{UP, DOWN, LEFT, RIGHT};
+
 int main(void)
 {
+	enum{WAREHOUSE, MOVEMENTS};
+
 	char *input_line = NULL;
 	size_t lim = 0;
 
+	char warehouse[BUF_SIZE][BUF_SIZE];
+	int move_arr[70000];
+	int move_arr_len = 0;
+	int parse_part = WAREHOUSE;
+	int warehouse_size = 0;
+
 	while(getline(&input_line, &lim, stdin) > 0){
+		if(input_line[0] == '\n'){
+			parse_part = MOVEMENTS;
+			continue;
+		}
+
+		if(parse_part == WAREHOUSE){
+			for(int i = 0; input_line[i] != '\n'; ++i){
+				warehouse[warehouse_size][i] = input_line[i];
+			}
+
+			++warehouse_size;
+		}else{
+			for(int i = 0; input_line[i] != '\n'; ++i){
+				switch(input_line[i]){
+				case '^':
+					move_arr[move_arr_len] = UP;
+					++move_arr_len;
+					break;
+				case 'v':
+					move_arr[move_arr_len] = DOWN;
+					++move_arr_len;
+					break;
+				case '<':
+					move_arr[move_arr_len] = LEFT;
+					++move_arr_len;
+					break;
+				case '>':
+					move_arr[move_arr_len] = RIGHT;
+					++move_arr_len;
+					break;
+				default:
+					printf("unrecognized char %c %d", input_line[i], input_line[i]);
+					break;
+				}
+			}
+		}
 	}
 
 	free(input_line);
