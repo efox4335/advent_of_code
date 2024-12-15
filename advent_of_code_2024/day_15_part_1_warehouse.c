3e9e268a3a2c7f69d2e31546e3cb87127aa6582a
Emma Christy <emma.t.christy@gmail.com>
2024-12-15 07:11:02 +0200

    added day 15 part 1 file

---
+++ advent_of_code_2024/day_15_part_1_warehouse.c
@@ -0,0 +1,18 @@
+/*
+ * just a simple simulation
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	while(getline(&input_line, &lim, stdin) > 0){
+	}
+
+	free(input_line);
+	return 0;
+}
