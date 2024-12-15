132e5ecda445ad82f33be806c969e255ad2dd377
Emma Christy <emma.t.christy@gmail.com>
2024-12-15 14:37:28 +0200

    added getting num

--- advent_of_code_2020/day_1_part_1_accounting.rs
+++ advent_of_code_2020/day_1_part_1_accounting.rs
@@ -3,11 +3,26 @@
  * record is num
 */
 
-use std::io::stdin;
+use std::{collections::HashMap, io::stdin};
 
 fn main() {
     let input: Vec<i32> = stdin()
         .lines()
         .map(|line| line.unwrap().parse::<i32>().unwrap())
         .collect();
+
+    let mut map = HashMap::<i32, i32>::new();
+    for num in input.iter() {
+        map.insert(2020 - num, *num);
+    }
+
+    for num in input {
+        match map.get(&num) {
+            Some(val) => {
+                println!("{}", val * num);
+                std::process::exit(0);
+            }
+            _ => (),
+        }
+    }
 }
