4c34dd475d584ddd1af31e69809d8ca04e6f538a
Emma Christy <emma.t.christy@gmail.com>
2024-12-15 07:54:38 +0200

    added function to simulate a move

--- advent_of_code_2024/day_15_part_1_warehouse.c
+++ advent_of_code_2024/day_15_part_1_warehouse.c
@@ -8,6 +8,58 @@
 enum{BUF_SIZE = 200};
 enum{UP, DOWN, LEFT, RIGHT};
 
+typedef struct{
+	int row;
+	int col;
+}cord;
+
+/*
+ * simulates moving boxes and the robot
+ * goes in dir marking everything as '.' keeping track of how many 'O' it comes accross till it hits '#'
+ * then it will write that amount of 'O' than the robot and update the robot_pos
+*/
+void sim_move(char warehouse[BUF_SIZE][BUF_SIZE], cord *robot_pos, const int dir)
+{
+	int row_dif = 0;
+	int col_dif = 0;
+
+	switch(dir){
+		case UP:
+			row_dif = -1;
+			break;
+		case DOWN:
+			row_dif = 1;
+			break;
+		case LEFT:
+			col_dif = -1;
+			break;
+		case RIGHT:
+			col_dif = 1;
+			break;
+	}
+
+	cord cur_pos = *robot_pos;
+	int box_count = 0;
+
+	while(warehouse[cur_pos.row][cur_pos.col] != '.'){
+		//no free space to move to
+		if(warehouse[cur_pos.row][cur_pos.col] == '#'){
+			return;
+		}
+		cur_pos.row += row_dif;
+		cur_pos.col += col_dif;
+	}
+
+	warehouse[robot_pos->row][robot_pos->col] = '.';
+
+	warehouse[cur_pos.row][cur_pos.col] = 'O';
+
+	robot_pos->row += row_dif;
+	robot_pos->col += col_dif;
+
+	warehouse[robot_pos->row][robot_pos->col] = '@';
+}
+
 int main(void)
 {
 	enum{WAREHOUSE, MOVEMENTS};
@@ -20,6 +72,7 @@ int main(void)
 	int move_arr_len = 0;
 	int parse_part = WAREHOUSE;
 	int warehouse_size = 0;
+	cord robot_pos;
 
 	while(getline(&input_line, &lim, stdin) > 0){
 		if(input_line[0] == '\n'){
@@ -29,6 +82,10 @@ int main(void)
 
 		if(parse_part == WAREHOUSE){
 			for(int i = 0; input_line[i] != '\n'; ++i){
+				if(input_line[i] == '@'){
+					robot_pos.row = warehouse_size;
+					robot_pos.col = i;
+				}
 				warehouse[warehouse_size][i] = input_line[i];
 			}
 
