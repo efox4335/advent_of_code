e119e73758748cba5e2838c19e53c6a8969bd0b3
Emma Christy <emma.t.christy@gmail.com>
2024-12-16 07:14:51 +0200

    added parsing input

--- advent_of_code_2024/day_16_part_1_maze.c
+++ advent_of_code_2024/day_16_part_1_maze.c
@@ -3,13 +3,22 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum{BUF_SIZE = 200};
+
 
 int main(void)
 {
 	char *input_line = NULL;
 	size_t lim = 0;
 
+	char input[BUF_SIZE][BUF_SIZE];
+	int line_count = 0;
+
 	while(getline(&input_line, &lim, stdin) > 1){
+		strcpy(input[line_count], input_line);
+		++line_count;
 	}
 
 	free(input_line);
