229d8e69de4ce4b57eac7ddf8cfbe85b2d5ae3b1
Emma Christy <emma.t.christy@gmail.com>
2024-12-18 18:35:00 +0200

    added day 3 part 2 file

---
+++ advent_of_code_2020/day_3_part_2_toboggan.rs
@@ -0,0 +1,74 @@
+/*
+ * use mod op to loop without needing to dupclate input
+ * add a loop with all slopes
+*/
+
+use std::io::stdin;
+
+struct Pos {
+    row: usize,
+    col: usize,
+}
+
+struct Slope {
+    row_dif: usize,
+    col_dif: usize,
+}
+
+fn main() {
+    let input: Vec<String> = stdin()
+        .lines()
+        .map(|s| s.unwrap().replace("\n", ""))
+        .collect();
+
+    let slopes: [Slope; 5] = [
+        Slope {
+            row_dif: 1,
+            col_dif: 1,
+        },
+        Slope {
+            row_dif: 1,
+            col_dif: 3,
+        },
+        Slope {
+            row_dif: 1,
+            col_dif: 5,
+        },
+        Slope {
+            row_dif: 1,
+            col_dif: 7,
+        },
+        Slope {
+            row_dif: 2,
+            col_dif: 1,
+        }
+    ];
+
+    let mut tree_prod = 1;
+
+    for slope in slopes{
+        let row_dif: usize = slope.row_dif;
+        let col_dif: usize = slope.col_dif;
+
+        let mut cur_pos: Pos = Pos { row: 0, col: 0 };
+        let mut tree_count = 0;
+
+        let col_len = input[0].chars().count();
+        let row_len = input.len();
+
+        while cur_pos.row < row_len {
+            if input[cur_pos.row].chars().nth(cur_pos.col).unwrap() != '.' {
+                tree_count += 1;
+            }
+
+            cur_pos.row += row_dif;
+            cur_pos.col += col_dif;
+
+            cur_pos.col %= col_len;
+        }
+
+        tree_prod *= tree_count;
+    }
+
+    println!("{}", tree_prod);
+}
