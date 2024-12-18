f3232805e291fcd995906e22a1b8cdea5e5952b2
Emma Christy <emma.t.christy@gmail.com>
2024-12-18 14:45:29 +0200

    solved day 2 part 1

--- advent_of_code_2020/day_2_part_1_password.rs
+++ advent_of_code_2020/day_2_part_1_password.rs
@@ -1,3 +1,29 @@
-fn main(){
+use std::io::stdin;
 
+fn main() {
+    let mut valid_pass = 0;
+
+    for line in stdin().lines() {
+        let temp = line.unwrap();
+        let mut sub_str = temp.split([' ', ':', '\n', '-']);
+
+        let lower_bound: i32 = sub_str.next().unwrap().parse().unwrap();
+        let upper_bound: i32 = sub_str.next().unwrap().parse().unwrap();
+
+        let letter: char = sub_str.next().unwrap().chars().nth(0).unwrap();
+
+        sub_str.next();
+        let count = sub_str
+            .next()
+            .unwrap()
+            .chars()
+            .filter(|c| *c as char == letter)
+            .count() as i32;
+
+        if count >= lower_bound && count <= upper_bound {
+            valid_pass += 1;
+        }
+    }
+
+    println!("{}", valid_pass);
 }
