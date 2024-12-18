1e6b66a41dbffb0e06ab43bd980aae91d2d956e4
Emma Christy <emma.t.christy@gmail.com>
2024-12-18 18:21:34 +0200

    added counting trees hit

--- advent_of_code_2020/day_3_part_1_toboggan.rs
+++ advent_of_code_2020/day_3_part_1_toboggan.rs
@@ -1,8 +1,35 @@
 use std::io::stdin;
 
+struct Pos {
+    row: usize,
+    col: usize,
+}
+
 fn main() {
     let input: Vec<String> = stdin()
         .lines()
         .map(|s| s.unwrap().replace("\n", ""))
         .collect();
+
+    const ROW_DIF: usize = 1;
+    const COL_DIF: usize = 3;
+
+    let mut cur_pos: Pos = Pos { row: 0, col: 0 };
+    let mut tree_count = 0;
+
+    let col_len = input[0].chars().count();
+    let row_len = input.len();
+
+    while cur_pos.row < row_len {
+        if input[cur_pos.row].chars().nth(cur_pos.col).unwrap() != '.' {
+            tree_count += 1;
+        }
+
+        cur_pos.row += ROW_DIF;
+        cur_pos.col += COL_DIF;
+
+        cur_pos.col %= col_len;
+    }
+
+    println!("{}", tree_count);
 }
