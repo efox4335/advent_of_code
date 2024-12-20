a06aefdcc3d53bb23978ebf1b9192cdeb3d5d3b1
Emma Christy <emma.t.christy@gmail.com>
2024-12-20 09:28:38 +0200

    added counting good cuts

--- advent_of_code_2024/day_20_part_1_cheat.c
+++ advent_of_code_2024/day_20_part_1_cheat.c
@@ -54,6 +54,74 @@ cord get_next(const int input[BUF_SIZE][BUF_SIZE], cord cur_pos, cord last_pos)
 	}
 }
 
+//gets time saved after wall at cut_pos is removed
+int get_cut_dif(const int input[BUF_SIZE][BUF_SIZE], const cord cut_pos, const int line_count)
+{
+	int min_path_len = 100000;
+	int max_path_len = -1;
+
+	const cord NORTH = {-1, 0};
+	const cord SOUTH = {1, 0};
+	const cord WEST = {0, -1};
+	const cord EAST = {0, 1};
+
+	cord next_pos;
+
+	//NORTH
+	next_pos.row = cut_pos.row + NORTH.row;
+	next_pos.col = cut_pos.col + NORTH.col;
+	if(next_pos.row >= 0 && input[next_pos.row][next_pos.col] >= 0){
+		if(input[next_pos.row][next_pos.col] < min_path_len){
+			min_path_len = input[next_pos.row][next_pos.col];
+		}
+
+		if(input[next_pos.row][next_pos.col] > max_path_len){
+			max_path_len = input[next_pos.row][next_pos.col];
+		}
+	}
+
+	//SOUTH
+	next_pos.row = cut_pos.row + SOUTH.row;
+	next_pos.col = cut_pos.col + SOUTH.col;
+	if(next_pos.row < line_count && input[next_pos.row][next_pos.col] >= 0){
+		if(input[next_pos.row][next_pos.col] < min_path_len){
+			min_path_len = input[next_pos.row][next_pos.col];
+		}
+
+		if(input[next_pos.row][next_pos.col] > max_path_len){
+			max_path_len = input[next_pos.row][next_pos.col];
+		}
+	}
+
+	//WEST
+	next_pos.row = cut_pos.row + WEST.row;
+	next_pos.col = cut_pos.col + WEST.col;
+	if(next_pos.col >= 0 && input[next_pos.row][next_pos.col] >= 0){
+		if(input[next_pos.row][next_pos.col] < min_path_len){
+			min_path_len = input[next_pos.row][next_pos.col];
+		}
+
+		if(input[next_pos.row][next_pos.col] > max_path_len){
+			max_path_len = input[next_pos.row][next_pos.col];
+		}
+	}
+
+	//EAST
+	next_pos.row = cut_pos.row + EAST.row;
+	next_pos.col = cut_pos.col + EAST.col;
+	if(next_pos.col < line_count && input[next_pos.row][next_pos.col] >= 0){
+		if(input[next_pos.row][next_pos.col] < min_path_len){
+			min_path_len = input[next_pos.row][next_pos.col];
+		}
+
+		if(input[next_pos.row][next_pos.col] > max_path_len){
+			max_path_len = input[next_pos.row][next_pos.col];
+		}
+	}
+
+	return (max_path_len - min_path_len) - 2;
+}
+
 int main(void)
 {
 	int input[BUF_SIZE][BUF_SIZE];
@@ -101,14 +169,36 @@ int main(void)
 	cur_pos.col = st_pos.col;
 	cord las_pos = cur_pos;
 
-	for(int i = 0; input[cur_pos.row][cur_pos.col] != END_VAL; ++i){
+	for(int i = 0; cur_pos.row != end_pos.row || cur_pos.col != end_pos.col; ++i){
 		cord next_pos = get_next(input, cur_pos, las_pos);
 
 		input[cur_pos.row][cur_pos.col] = i;
 
 		las_pos = cur_pos;
 		cur_pos = next_pos;
+
+		input[cur_pos.row][cur_pos.col] = i + 1;
 	}
+
+	int good_cut = 0;
+
+	for(int i = 0; i < line_count; ++i){
+		for(int j = 0; j < line_count; ++j){
+			if(input[i][j] == WALL){
+				cord temp;
+				temp.row = i;
+				temp.col = j;
+
+				int cut_len = get_cut_dif(input, temp, line_count);
+
+				if(cut_len >= 100){
+					++good_cut;
+				}
+			}
+		}
+	}
+
+	printf("%d\n", good_cut);
 
 	free(input_line);
 	return 0;
