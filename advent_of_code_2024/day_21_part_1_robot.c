1fc7a14bca0590e2f759cfc4dc5be7f5221db10f
Emma Christy <emma.t.christy@gmail.com>
2024-12-21 08:07:10 +0200

    added function to get directions for keypad buttons

--- advent_of_code_2024/day_21_part_1_robot.c
+++ advent_of_code_2024/day_21_part_1_robot.c
@@ -31,6 +31,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+	int row;
+	int col;
+}cord;
+
+/*
+ * if row is > 0 dir is down otherwise up
+ * if col is > 0 dir is right otherwise left
+ * returns how to get from button_1 to button_2
+*/
+cord get_keypad_dist(char button_1, char button_2)
+{
+	const cord seven = {0, 0};
+	const cord eight = {0, 1};
+	const cord nine = {0, 2};
+	const cord four = {1, 0};
+	const cord five = {1, 1};
+	const cord six = {1, 2};
+	const cord one = {2, 0};
+	const cord two = {2, 1};
+	const cord three = {2, 2};
+	const cord zero = {3, 1};
+	const cord A = {3, 2};
+
+	cord button_1_loc;
+
+	switch(button_1){
+	case '0':
+		button_1_loc = zero;
+		break;
+	case '1':
+		button_1_loc = one;
+		break;
+	case '2':
+		button_1_loc = two;
+		break;
+	case '3':
+		button_1_loc = three;
+		break;
+	case '4':
+		button_1_loc = four;
+		break;
+	case '5':
+		button_1_loc = five;
+		break;
+	case '6':
+		button_1_loc = six;
+		break;
+	case '7':
+		button_1_loc = seven;
+		break;
+	case '8':
+		button_1_loc = eight;
+		break;
+	case '9':
+		button_1_loc = nine;
+		break;
+	case 'A':
+		button_1_loc = A;
+		break;
+	default:
+		printf("get_keypad_dist error button_1 bad key %c\n", button_1);
+		exit(1);
+	}
+
+	cord button_2_loc;
+
+	switch(button_2){
+	case '0':
+		button_2_loc = zero;
+		break;
+	case '1':
+		button_2_loc = one;
+		break;
+	case '2':
+		button_2_loc = two;
+		break;
+	case '3':
+		button_2_loc = three;
+		break;
+	case '4':
+		button_2_loc = four;
+		break;
+	case '5':
+		button_2_loc = five;
+		break;
+	case '6':
+		button_2_loc = six;
+		break;
+	case '7':
+		button_2_loc = seven;
+		break;
+	case '8':
+		button_2_loc = eight;
+		break;
+	case '9':
+		button_2_loc = nine;
+		break;
+	case 'A':
+		button_2_loc = A;
+		break;
+	default:
+		printf("get_keypad_dist error button_2 bad key %c\n", button_2);
+		exit(1);
+	}
+
+	cord dir = {button_2_loc.row - button_1_loc.row, button_2_loc.col - button_1_loc.col};
+
+	return dir;
+}
+
 int main(void)
 {
 	char *input_line = NULL;
