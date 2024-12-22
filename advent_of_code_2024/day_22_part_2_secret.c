e4adb4c9627804aead11354455e4403ea6a9851b
Emma Christy <emma.t.christy@gmail.com>
2024-12-22 07:59:53 +0200

    added day 22 part 2 file

---
+++ advent_of_code_2024/day_22_part_2_secret.c
@@ -0,0 +1,43 @@
+/*
+ * sn = (((s << 6) ^ s) % (1 << 24))
+ * sf = (((sn >> 5) ^ sn) % (1 << 24))
+ * sk = (((sf << 11) ^ sf) % (1 << 24))
+ * s + 1 = sk
+ * just a simple calculation to calculate the 2000th secret number
+ *
+ * store each possable combinations of differences in an array along with the total sum for that sequence and weather it has been seen for this seed to avoid counting more than once
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+long get_next_num(long cur_num)
+{
+	cur_num = (((cur_num << 6) ^ cur_num) % (1 << 24));
+	cur_num = (((cur_num >> 5) ^ cur_num) % (1 << 24));
+	cur_num = (((cur_num << 11) ^ cur_num) % (1 << 24));
+
+	return cur_num;
+}
+
+int main(void)
+{
+	char *input_line = NULL;
+	size_t lim = 0;
+
+	long secret_num_sum = 0;
+
+	while(getline(&input_line, &lim, stdin) > 1){
+		long num = atol(input_line);
+
+		for(int i = 0; i < 2000; ++i){
+			num = get_next_num(num);
+		}
+
+		secret_num_sum += num;
+	}
+
+	printf("%ld\n", secret_num_sum);
+
+	free(input_line);
+	return 0;
+}
