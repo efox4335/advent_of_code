43fd1f65abe4bcbf7ee4eae6bd35c051ddbe34ad
Emma Christy <emma.t.christy@gmail.com>
2024-12-22 08:21:15 +0200

    solved day 22 part 2

--- advent_of_code_2024/day_22_part_2_secret.c
+++ advent_of_code_2024/day_22_part_2_secret.c
@@ -19,24 +19,68 @@ long get_next_num(long cur_num)
 	return cur_num;
 }
 
+typedef struct{
+	long sum;
+	long last_arr_seen;
+}seq_sum;
+
 int main(void)
 {
 	char *input_line = NULL;
 	size_t lim = 0;
 
-	long secret_num_sum = 0;
+	seq_sum seq_arr[20][20][20][20];
+
+	for(int i = 0; i < 20; ++i){
+		for(int j = 0; j < 20; ++j){
+			for(int k = 0; k < 20; ++k){
+				for(int l = 0; l < 20; ++l){
+					seq_arr[i][j][k][l].sum = 0;
+					seq_arr[i][j][k][l].last_arr_seen = -1;
+				}
+			}
+		}
+	}
+
+	int cur_seed = 0;
+	long largest_sum = 0;
 
 	while(getline(&input_line, &lim, stdin) > 1){
 		long num = atol(input_line);
 
+		int cur_seq[4] = {0, 0, 0, 0};
+		int seq_ind = 0;
 		for(int i = 0; i < 2000; ++i){
-			num = get_next_num(num);
+			long next_num = get_next_num(num);
+
+			long cur_seq_pt = ((next_num % 10) - (num % 10)) + 9;
+
+			if(seq_ind >= 4){
+				cur_seq[0] = cur_seq[1];
+				cur_seq[1] = cur_seq[2];
+				cur_seq[2] = cur_seq[3];
+				cur_seq[3] = cur_seq_pt;
+
+				if(seq_arr[cur_seq[0]][cur_seq[1]][cur_seq[2]][cur_seq[3]].last_arr_seen != cur_seed){
+					seq_arr[cur_seq[0]][cur_seq[1]][cur_seq[2]][cur_seq[3]].last_arr_seen = cur_seed;
+					seq_arr[cur_seq[0]][cur_seq[1]][cur_seq[2]][cur_seq[3]].sum += (next_num % 10);
+
+					if(seq_arr[cur_seq[0]][cur_seq[1]][cur_seq[2]][cur_seq[3]].sum > largest_sum){
+						largest_sum = seq_arr[cur_seq[0]][cur_seq[1]][cur_seq[2]][cur_seq[3]].sum;
+					}
+				}
+			}else{
+				cur_seq[seq_ind] = cur_seq_pt;
+				++seq_ind;
+			}
+
+			num = next_num;
 		}
 
-		secret_num_sum += num;
+		++cur_seed;
 	}
 
-	printf("%ld\n", secret_num_sum);
+	printf("%ld\n", largest_sum);
 
 	free(input_line);
 	return 0;
