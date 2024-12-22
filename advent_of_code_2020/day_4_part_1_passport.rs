f0c5cc152602c3f30939e07c298f8277bcd2e57f
Emma Christy <emma.t.christy@gmail.com>
2024-12-22 09:54:55 +0200

    added getting valid passport count

--- advent_of_code_2020/day_4_part_1_passport.rs
+++ advent_of_code_2020/day_4_part_1_passport.rs
@@ -4,6 +4,49 @@
 
 use std::io::stdin;
 
+struct Fields {
+    byr: bool,
+    iyr: bool,
+    eyr: bool,
+    hgt: bool,
+    hcl: bool,
+    ecl: bool,
+    pid: bool,
+    cid: bool,
+}
+
+impl Default for Fields {
+    fn default() -> Self {
+        Self {
+            byr: Default::default(),
+            iyr: Default::default(),
+            eyr: Default::default(),
+            hgt: Default::default(),
+            hcl: Default::default(),
+            ecl: Default::default(),
+            pid: Default::default(),
+            cid: Default::default(),
+        }
+    }
+}
+
+impl Fields {
+    fn is_valid(&self) -> bool {
+        if self.byr == true
+            && self.iyr == true
+            && self.eyr == true
+            && self.hgt == true
+            && self.hcl == true
+            && self.ecl == true
+            && self.pid == true
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
+
 fn main() {
     let mut passports: Vec<String> = vec!["".to_string()];
 
@@ -15,4 +58,29 @@ fn main() {
             passports.last_mut().unwrap().push_str(" ");
         }
     }
+
+    let mut valid_pass_count = 0;
+
+    for passport in passports {
+        let mut cur_fields: Fields = Fields::default();
+        for fields in passport.split([' ', ':']) {
+            match fields {
+                "byr" => cur_fields.byr = true,
+                "iyr" => cur_fields.iyr = true,
+                "eyr" => cur_fields.eyr = true,
+                "hgt" => cur_fields.hgt = true,
+                "hcl" => cur_fields.hcl = true,
+                "ecl" => cur_fields.ecl = true,
+                "pid" => cur_fields.pid = true,
+                "cid" => cur_fields.cid = true,
+                _ => (),
+            }
+        }
+
+        if cur_fields.is_valid(){
+            valid_pass_count += 1;
+        }
+    }
+
+    println!("{}", valid_pass_count);
 }
