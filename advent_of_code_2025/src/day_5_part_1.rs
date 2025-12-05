5adaa39a931e2c9e40f5735367de2e35d2d48cba
emma <emma.t.christy@gmail.com>
2025-12-05 10:31:47 +0200

    solved day 5 part 1

---
+++ advent_of_code_2025/src/day_5_part_1.rs
@@ -0,0 +1,195 @@
+use std::{collections::BTreeSet, io};
+
+enum Part {
+    Ranges,
+    Foods,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Range {
+    lower: u64,
+    upper: u64,
+}
+
+#[derive(Debug)]
+enum RangeKey {
+    Range(Range),
+    Food(u64),
+}
+
+impl Ord for RangeKey {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self {
+            RangeKey::Range(range) => match other {
+                RangeKey::Range(other_range) => {
+                    if range.upper < other_range.lower {
+                        return std::cmp::Ordering::Less;
+                    }
+
+                    if range.lower > other_range.upper {
+                        return std::cmp::Ordering::Greater;
+                    }
+
+                    return std::cmp::Ordering::Equal;
+                }
+                RangeKey::Food(other_food_id) => {
+                    let other_food_id = *other_food_id;
+
+                    if other_food_id > range.upper {
+                        return std::cmp::Ordering::Less;
+                    }
+
+                    if other_food_id < range.lower {
+                        return std::cmp::Ordering::Greater;
+                    }
+
+                    return std::cmp::Ordering::Equal;
+                }
+            },
+            RangeKey::Food(food_id) => {
+                let food_id = *food_id;
+                match other {
+                    RangeKey::Range(other_range) => {
+                        if food_id > other_range.upper {
+                            return std::cmp::Ordering::Greater;
+                        }
+
+                        if food_id < other_range.lower {
+                            return std::cmp::Ordering::Less;
+                        }
+
+                        return std::cmp::Ordering::Equal;
+                    }
+                    RangeKey::Food(other_food_id) => {
+                        let other_food_id = *other_food_id;
+                        panic!("this should never happen {food_id} {other_food_id}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl PartialOrd for RangeKey {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for RangeKey {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            RangeKey::Range(range) => match other {
+                RangeKey::Range(other_range) => {
+                    if range.upper < other_range.lower || range.lower > other_range.upper {
+                        return false;
+                    }
+
+                    return true;
+                }
+                RangeKey::Food(other_food_id) => {
+                    let other_food_id = *other_food_id;
+
+                    if other_food_id > range.upper || other_food_id < range.lower {
+                        return false;
+                    }
+
+                    return true;
+                }
+            },
+            RangeKey::Food(food_id) => {
+                let food_id = *food_id;
+                match other {
+                    RangeKey::Range(other_range) => {
+                        if food_id > other_range.upper || food_id < other_range.lower {
+                            return false;
+                        }
+
+                        return true;
+                    }
+                    RangeKey::Food(other_food_id) => {
+                        let other_food_id = *other_food_id;
+                        panic!("this should never happen {food_id} {other_food_id}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl Range {
+    fn merge(&self, other: &Self) -> Range {
+        let lower = if self.lower > other.lower {
+            other.lower
+        } else {
+            self.lower
+        };
+        let upper = if self.upper < other.upper {
+            other.upper
+        } else {
+            self.upper
+        };
+
+        Range { lower, upper }
+    }
+}
+
+impl Eq for RangeKey {}
+
+pub fn solver() {
+    let mut part = Part::Ranges;
+
+    let mut foods: Vec<RangeKey> = Vec::new();
+
+    let mut ranges: BTreeSet<RangeKey> = BTreeSet::new();
+
+    for line in io::stdin().lines() {
+        let line = line.unwrap();
+
+        if line.len() == 0 {
+            part = Part::Foods;
+
+            continue;
+        }
+
+        match part {
+            Part::Ranges => {
+                let mut range_vals = line.split(['-', '\n']);
+
+                let lower: u64 = range_vals.next().unwrap().parse().unwrap();
+                let upper: u64 = range_vals.next().unwrap().parse().unwrap();
+
+                let mut range = Range { lower, upper };
+
+                loop {
+                    match ranges.take(&RangeKey::Range(range)) {
+                        Some(pot_range) => match pot_range {
+                            RangeKey::Range(merge_range) => {
+                                range = merge_range.merge(&range);
+                            }
+                            RangeKey::Food(food_id) => {
+                                panic!("this should never happen {food_id}");
+                            }
+                        },
+                        None => {
+                            ranges.insert(RangeKey::Range(range));
+
+                            break;
+                        }
+                    }
+                }
+            }
+            Part::Foods => foods.push(RangeKey::Food(line.parse().unwrap())),
+        }
+    }
+
+    let fresh_ids = foods.iter().fold(0, |acc, food_id| {
+        if ranges.contains(food_id) {
+            return acc + 1;
+        }
+
+        acc
+    });
+
+    println!("{fresh_ids}");
+}
--- advent_of_code_2025/src/main.rs
+++ advent_of_code_2025/src/main.rs
@@ -8,6 +8,7 @@ mod day_3_part_1;
 mod day_3_part_2;
 mod day_4_part_1;
 mod day_4_part_2;
+mod day_5_part_1;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -24,6 +25,7 @@ fn main() {
         "d3p2" => day_3_part_2::solver(),
         "d4p1" => day_4_part_1::solver(),
         "d4p2" => day_4_part_2::solver(),
+        "d5p1" => day_5_part_1::solver(),
         _ => panic!("unknown day/part"),
     }
 }
